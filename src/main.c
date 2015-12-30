115526bb6d1d62ba3fa736016a5097e49f4e6ec7
Autif Khan <autif_khan@bose.com>
2015-12-30 15:12:52 -0500

    Fixed spelling mistake. Updated README with the right number of Hi Added utility function add()

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+int add( int a, int b);
+
 int main() {
     printf( "Hi!\n" );
-    printf( "Hisjn!\n" );
+    printf( "Hi! Hi!\n" );
+    printf( "add(20, 25) = %d\n", add( 20, 25) );
     return 0;
 }
 
