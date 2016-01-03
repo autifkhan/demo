f4b667efef88d8043b8d5e4da1529b339ed74686
Hamza Tariq Khan <hamzatariqkhan94@gmail.com>
2016-01-03 20:28:37 +0530

    Modified README and main.c file

--- src/main.c
+++ src/main.c
@@ -3,8 +3,7 @@
 int add( int a, int b);
 
 int main() {
-    printf( "Hi!\n" );
-    printf( "Hi! Hi!\n" );
+    printf( "Hi World!\n" );
     printf( "Added a new line!\n" );
     printf( "add(20, 25) = %d\n", add( 20, 25) );
     return 0;
