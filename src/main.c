f28ba558abe7083ae5f4a40401bf7e7b9bbbaea2
Hamza Tariq Khan <hamzatariqkhan94@gmail.com>
2016-01-03 20:42:30 +0530

    Fixed hello print statement

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 int add( int a, int b);
 
 int main() {
-    printf( "Hi World!\n" );
+    printf( "Hello World!\n" );
     printf( "Added a new line!\n" );
     printf( "add(20, 25) = %d\n", add( 20, 25) );
     return 0;
