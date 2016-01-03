505f894679179065e67e2f3ce8cdc1f65f62a2b2
Hamza Tariq Khan <hamzatariqkhan94@gmail.com>
2016-01-03 20:15:46 +0530

    Added a new print statement in file main.c

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@ int add( int a, int b);
 int main() {
     printf( "Hi!\n" );
     printf( "Hi! Hi!\n" );
+    printf( "Added a new line!\n" );
     printf( "add(20, 25) = %d\n", add( 20, 25) );
     return 0;
 }
